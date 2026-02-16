835cff58da11269999488fe4cb9c0eab640712f6
nekithrill <nekithrill@gmail.com>
2026-02-16 16:59:08 +0200

    remove all icons stuff (due to lucide-react usage)

--- src/shared/components/icons/Logo.tsx
+++
@@ -1,6 +0,0 @@
-import logo from '@/shared/assets/logo.svg'
-import styles from '@/widgets/footer/Footer.module.scss'
-
-export const Logo = () => (
-	<img src={logo} className={styles['logo']} alt='Logo' />
-)
