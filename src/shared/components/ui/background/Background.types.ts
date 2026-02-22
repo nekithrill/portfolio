4e8285c5a9a4925461c878cb10cfd0e1fbf31261
nekithrill <nekithrill@gmail.com>
2026-02-22 13:45:36 +0200

    refactor: types usage improve & old feature remove
    - Type modules remove (moved to its modules)
    - Remove Marker feature (not needed)

--- src/shared/components/ui/background/Background.types.ts
+++
@@ -1,7 +0,0 @@
-export interface BackgroundProps {
-	cellSize?: number
-	lineColor?: string
-	speed?: number
-	blur?: boolean
-	blurAmount?: number
-}
--- src/shared/components/ui/section/Section.types.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react'
-
-export interface SectionProps {
-	useH1?: boolean
-	title: string
-	children: ReactNode
-	className?: string
-	footerInfo?: ReactNode
-}
