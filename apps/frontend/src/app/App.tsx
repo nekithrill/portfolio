9f07e1b2d937aabf9b2c8b3762b90fe1de63e678
nekithrill <nekithrill@gmail.com>
2026-02-22 13:53:32 +0200

    feat: add new routes

---
+++ apps/frontend/src/app/App.tsx
@@ -0,0 +1,35 @@
+import { MainLayout } from '@/app/layouts/MainLayout'
+import '@/app/styles/main.scss'
+import { AboutPage } from '@/pages/about/AboutPage'
+import { AdminPage } from '@/pages/admin/AdminPage'
+import { LoginPage } from '@/pages/auth/LoginPage'
+import { RegisterPage } from '@/pages/auth/RegisterPage'
+import { BlogPage } from '@/pages/blog/BlogPage'
+import { ContactsPage } from '@/pages/contacts/ContactsPage'
+import { HomePage } from '@/pages/home/HomePage'
+import { PrivacyPage } from '@/pages/privacy/PrivacyPage'
+import { ProjectsPage } from '@/pages/projects/ProjectsPage'
+import { SkillsPage } from '@/pages/skills/SkillsPage'
+import { ProfilePage } from '@/pages/user/ProfilePage'
+import '@/shared/configs/i18n/config'
+import { Route, Routes } from 'react-router-dom'
+
+export default function App() {
+	return (
+		<Routes>
+			<Route path='/' element={<MainLayout />}>
+				<Route index element={<HomePage />} />
+				<Route path='admin' element={<AdminPage />} />
+				<Route path='auth/login' element={<LoginPage />} />
+				<Route path='auth/register' element={<RegisterPage />} />
+				<Route path='profile' element={<ProfilePage />} />
+				<Route path='about' element={<AboutPage />} />
+				<Route path='skills' element={<SkillsPage />} />
+				<Route path='projects' element={<ProjectsPage />} />
+				<Route path='blog' element={<BlogPage />} />
+				<Route path='contacts' element={<ContactsPage />} />
+				<Route path='privacy' element={<PrivacyPage />} />
+			</Route>
+		</Routes>
+	)
+}
