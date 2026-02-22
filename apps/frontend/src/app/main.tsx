479000261d87df4fcc76d241b2d7331b9b41cfd2
nekithrill <nekithrill@gmail.com>
2026-02-22 13:54:00 +0200

    refactor: some code improvements

---
+++ apps/frontend/src/app/main.tsx
@@ -0,0 +1,18 @@
+import App from '@/app/App'
+import { RootProvider } from '@/app/providers/RootProvider'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
+const container = document.getElementById('root')
+if (!container) throw new Error('Root container missing in index.html')
+
+createRoot(container).render(
+	<StrictMode>
+		<BrowserRouter>
+			<RootProvider>
+				<App />
+			</RootProvider>
+		</BrowserRouter>
+	</StrictMode>
+)
