ed0e72c4a403d2b11183b662832af09af225e318
nekithrill <nekithrill@gmail.com>
2026-03-03 14:55:00 +0200

    refactor: create index.ts files for all modules to adjust imports
    - create index.ts for @/shared/components/ui
    - create index.ts for @/widgets

---
+++ apps/frontend/src/shared/components/ui/card/index.ts
@@ -0,0 +1,11 @@
+export { Card } from './Card'
+export { EducationCard } from './variants/education-card/EducationCard'
+export { ProjectCard } from './variants/project-card/ProjectCard'
+
+export type { CardProps } from './Card.types'
+export type { EducationCardProps } from './variants/education-card/EducationCard.types'
+export type {
+	ProjectCardLink,
+	ProjectCardProps,
+	ProjectCardTag
+} from './variants/project-card/ProjectCard.types'
---
+++ apps/frontend/src/shared/components/ui/file-container/index.ts
@@ -0,0 +1,1 @@
+export { FileContainer } from './FileContainer'
