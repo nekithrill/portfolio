08fec03c64f6f28a413377dad88e739b78a6736c
nekithrill <nekithrill@gmail.com>
2026-03-08 12:22:03 +0200

    refactor: little improvements

--- apps/frontend/src/app/providers/RootProvider.tsx
+++ apps/frontend/src/app/providers/RootProvider.tsx
@@ -1,6 +1,6 @@
 import { LanguageProvider } from '@/app/providers/language'
 import { ThemeProvider } from '@/app/providers/theme'
-import { ReactNode } from 'react'
+import type { ReactNode } from 'react'
 
 interface RootProviderProps {
 	children: ReactNode
