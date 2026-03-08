73fbe62e067b819719778aca2e17b4b5b1838f5a
nekithrill <nekithrill@gmail.com>
2026-03-08 12:21:43 +0200

    chore: uncomment auth pages

--- apps/frontend/src/app/App.tsx
+++ apps/frontend/src/app/App.tsx
@@ -1,17 +1,18 @@
 import { RootLayout } from '@/app/layouts/RootLayout'
 import '@/app/styles/main.scss'
 import { AboutPage } from '@/pages/about/AboutPage'
-// import { AdminPage } from '@/pages/admin/AdminPage'
-// import { LoginPage } from '@/pages/auth/LoginPage'
-// import { RegisterPage } from '@/pages/auth/RegisterPage'
-// import { ProfilePage } from '@/pages/user/ProfilePage'
+import { AdminPage } from '@/pages/admin/AdminPage'
+import { LoginPage } from '@/pages/auth/login/LoginPage'
+import { RegisterPage } from '@/pages/auth/register/RegisterPage'
 import { BlogPage } from '@/pages/blog/BlogPage'
 import { ContactsPage } from '@/pages/contacts/ContactsPage'
 import { HomePage } from '@/pages/home/HomePage'
+import { PostPage } from '@/pages/post/PostPage'
 import { PrivacyPage } from '@/pages/privacy/PrivacyPage'
 import { ProjectsPage } from '@/pages/projects/ProjectsPage'
 import { SkillsPage } from '@/pages/skills/SkillsPage'
 import { TermsPage } from '@/pages/terms/TermsPage'
+import { ProfilePage } from '@/pages/user/ProfilePage'
 import '@/shared/configs/i18n/config'
 import { Route, Routes } from 'react-router-dom'
 
@@ -20,14 +21,15 @@ export default function App() {
 		<Routes>
 			<Route path='/' element={<RootLayout />}>
 				<Route index element={<HomePage />} />
-				{/* <Route path='admin' element={<AdminPage />} /> */}
-				{/* <Route path='auth/login' element={<LoginPage />} /> */}
-				{/* <Route path='auth/register' element={<RegisterPage />} /> */}
-				{/* <Route path='profile' element={<ProfilePage />} /> */}
+				<Route path='admin' element={<AdminPage />} />
+				<Route path='auth/login' element={<LoginPage />} />
+				<Route path='auth/register' element={<RegisterPage />} />
+				<Route path='profile' element={<ProfilePage />} />
 				<Route path='about' element={<AboutPage />} />
 				<Route path='skills' element={<SkillsPage />} />
 				<Route path='projects' element={<ProjectsPage />} />
 				<Route path='blog' element={<BlogPage />} />
+				<Route path='blog/:slug' element={<PostPage />} />
 				<Route path='contacts' element={<ContactsPage />} />
 				<Route path='privacy-policy' element={<PrivacyPage />} />
 				<Route path='terms-of-service' element={<TermsPage />} />
