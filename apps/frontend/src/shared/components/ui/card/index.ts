e0b321192c0d062b78c619aae043f570ddac5e96
nekithrill <nekithrill@gmail.com>
2026-03-08 12:52:06 +0200

    chore(ui): add all exports to index.ts (card)

--- apps/frontend/src/shared/components/ui/card/index.ts
+++ apps/frontend/src/shared/components/ui/card/index.ts
@@ -1,9 +1,11 @@
 export { Card } from './Card'
 export { EducationCard } from './variants/education-card/EducationCard'
+export { PostCard } from './variants/post-card/PostCard'
 export { ProjectCard } from './variants/project-card/ProjectCard'
 
 export type { CardProps } from './Card.types'
 export type { EducationCardProps } from './variants/education-card/EducationCard.types'
+export type { PostCardProps } from './variants/post-card/PostCard.types'
 export type {
 	ProjectCardLink,
 	ProjectCardProps,
