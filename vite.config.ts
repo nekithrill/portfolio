0d4dc2e60d9adeef1fe8fc7b6e09d0d5d29bdec4
nekithrill <nekithrill@gmail.com>
2026-04-07 21:58:42 +0300

    chore: configs setup
    - stylelint config
    - biomejs config
    - tsconfigs
    - vite config

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src')
+		}
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				loadPaths: [path.resolve(__dirname, './src')]
+			}
+		}
+	},
+	server: {
+		port: Number(process.env.VITE_PORT) || 3000,
+		host: true
+	}
+})
