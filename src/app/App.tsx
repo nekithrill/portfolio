b354371f614e2ea5534c9bc5e8db4cb56e85a208
nekithrill <nekithrill@gmail.com>
2026-05-08 16:38:42 +0300

    refactor: move i18n and styles initialization from App.tsx to  main.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,7 @@
 import { RootLayout } from '@/app/layouts/RootLayout'
-import '@/app/styles/main.scss'
 import { BioSection } from '@/sections/bio/BioSection'
 import { ProjectsSection } from '@/sections/projects/ProjectsSection'
 import { SkillsSection } from '@/sections/skills/SkillsSection'
-import '@/shared/configs/i18n/config'
 
 export default function App() {
 	return (
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,5 +1,7 @@
 import App from '@/app/App'
 import { RootProvider } from '@/app/providers/RootProvider'
+import '@/app/styles/main.scss'
+import '@/shared/configs/i18n/config'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
