35e99c2ccafea75c3db01c779ff6e05146c2e3ad
nekithrill <nekithrill@gmail.com>
2026-05-08 16:53:30 +0300

    chore: Links component moved from /widgets to shared/components/ui

--- src/shared/components/ui/links/LinksData.ts
+++ src/shared/components/ui/links/LinksData.ts
@@ -1,4 +1,4 @@
-import { IconType } from 'react-icons'
+import type { IconType } from 'react-icons'
 import { FiGithub, FiLinkedin } from 'react-icons/fi'
 import { RiTelegram2Line } from 'react-icons/ri'
 
