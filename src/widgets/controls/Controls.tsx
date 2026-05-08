f726ddde64e332b99686c08b7c63f2bf54fa9bdd
nekithrill <nekithrill@gmail.com>
2026-05-08 16:55:25 +0300

    refactor: Controls widget simplified

--- src/widgets/controls/Controls.tsx
+++ src/widgets/controls/Controls.tsx
@@ -1,16 +1,12 @@
-import { Clock } from '@/widgets/clock'
-import { LocaleSwitch } from '@/widgets/locale-switch'
-import { ThemeSwitch } from '@/widgets/theme-switch'
+import { LocaleSwitch } from '@/features/locale-switch'
+import { ThemeSwitch } from '@/features/theme-switch'
 import styles from './Controls.module.scss'
 
 export const Controls = () => {
 	return (
 		<div className={styles['controls']}>
-			<Clock />
-			<div className={styles['controls__switches']}>
-				<ThemeSwitch />
-				<LocaleSwitch />
-			</div>
+			<ThemeSwitch />
+			<LocaleSwitch />
 		</div>
 	)
 }
