4798301d2ab802f40766b6583f028f2f65c3c100
nekithrill <nekithrill@gmail.com>
2026-05-08 16:42:27 +0300

    refactor: interface moved to shared/types/local, change undefined -> null

--- src/app/providers/language/LanguageContext.ts
+++ src/app/providers/language/LanguageContext.ts
@@ -1,11 +1,4 @@
-import type { Language } from '@/shared/types/locale'
+import type { LanguageSwitchResult } from '@/shared/types/locale'
 import { createContext } from 'react'
 
-export interface LanguageContextType {
-	language: Language
-	setLanguage: (lang: Language) => void
-}
-
-export const LanguageContext = createContext<LanguageContextType | undefined>(
-	undefined
-)
+export const LanguageContext = createContext<LanguageSwitchResult | null>(null)
