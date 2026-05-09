3a77ad9509d4d8370f7dcddfb6158043f622ce4a
nekithrill <nekithrill@gmail.com>
2026-05-09 22:02:11 +0300

    fix: change href to /portfolio

--- src/shared/components/ui/logo/Logo.tsx
+++ src/shared/components/ui/logo/Logo.tsx
@@ -2,7 +2,7 @@ import logo from '@/shared/assets/logo.svg'
 import styles from '@/shared/components/ui/logo/Logo.module.scss'
 
 export const Logo = () => (
-	<a href='/'>
+	<a href='/portfolio'>
 		<img src={logo} className={styles['logo']} alt='nekithrill logo' />
 	</a>
 )
