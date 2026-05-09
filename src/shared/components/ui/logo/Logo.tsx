08d30eda17164a3a810e5145b51d39e914c35202
nekithrill <nekithrill@gmail.com>
2026-05-09 17:36:05 +0300

    feat: now Logo component wrapped with <a> for navigate to /

--- src/shared/components/ui/logo/Logo.tsx
+++ src/shared/components/ui/logo/Logo.tsx
@@ -2,5 +2,7 @@ import logo from '@/shared/assets/logo.svg'
 import styles from '@/shared/components/ui/logo/Logo.module.scss'
 
 export const Logo = () => (
-	<img src={logo} className={styles['logo']} alt='nekithrill logo' />
+	<a href='/'>
+		<img src={logo} className={styles['logo']} alt='nekithrill logo' />
+	</a>
 )
