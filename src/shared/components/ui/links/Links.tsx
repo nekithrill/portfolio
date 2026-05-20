41e379ec167ed736527eefe4ac760d2bf23fff7c
nekithrill <nekithrill@gmail.com>
2026-05-20 15:25:37 +0300

    cleanup: delete data file (data move directly to component)

--- src/shared/components/ui/links/Links.tsx
+++ src/shared/components/ui/links/Links.tsx
@@ -1,12 +1,42 @@
+import type { IconType } from 'react-icons'
+import { FiGithub, FiLinkedin } from 'react-icons/fi'
+import { RiTelegram2Line } from 'react-icons/ri'
 import styles from './Links.module.scss'
-import { linksData } from './LinksData'
+
+interface LinkItem {
+	key: string
+	href: string
+	label: string
+	Icon: IconType
+}
 
 const ICON_SIZE = '2rem'
+
+const links: LinkItem[] = [
+	{
+		key: 'github',
+		href: 'https://github.com/nekithrill',
+		label: 'GitHub',
+		Icon: FiGithub
+	},
+	{
+		key: 'telegram',
+		href: 'https://t.me/nekithrill',
+		label: 'Telegram',
+		Icon: RiTelegram2Line
+	},
+	{
+		key: 'linkedin',
+		href: 'https://linkedin.com/in/nekithrill',
+		label: 'LinkedIn',
+		Icon: FiLinkedin
+	}
+]
 
 export const Links = () => {
 	return (
 		<nav className={styles['links']}>
-			{linksData.map(({ key, href, label, Icon }) => (
+			{links.map(({ key, href, label, Icon }) => (
 				<a
 					key={key}
 					href={href}
